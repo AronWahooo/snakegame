25e7ee6d769cd36299abb0faec593f3b78933709
AronWahooo <158843881+AronWahooo@users.noreply.github.com>
2024-02-06 11:32:24 +0545

    Changed labels to switches

--- snakegame.c
+++ snakegame.c
@@ -2,73 +2,67 @@
 #include<stdlib.h>
 #include<conio.h>
 int height=20,width=20,x,y,xf,yf,k,m1,end,i,j;
-void setup()
-{
-	x=height/2;
-	y=width/2;
-		label1:
-		xf=rand()%height;
-		if(xf == 0)
-		goto label1;
-		label2:
-		yf=rand()%width;
-		if(yf == 0) 
-		goto label2;
+void setup() {
+    x = height / 2;
+    y = width / 2;
+
+    while (1) {
+        switch (rand() % 2) {
+            case 0:
+                xf = rand() % height;
+                if (xf != 0)
+                    break;
+            case 1:
+                yf = rand() % width;
+                if (yf != 0)
+                    break;
+        }
+        break;
+    }
 }
-void draw()
-{
-	system("cls");
-	for(i=0;i<=height;i++)
-	{
-		for(j=0;j<=width;j++)
-	 		{
-	 			if (i==height && j==0)
-					{
-						printf("|");
-					}
-				else if (i==height && j==width)
-					{
-						printf("|");
-					}
-	 			else if(i==0 || i==height)
-	 				{
-	 					printf("_");
-					}
-				else if(j==0 || j==width)
-					{
-						printf("|");
-					}
-				else 
-					{
-						if(i==x && j==y)
-						{
-							printf("O");
-						}
-						else if (i==xf && j==yf)
-						{
-							printf("#");
-						}
-						else
-						printf(" ");
-					}
-	 		}
-	 		printf("\n");
-	}
-	if(x==height || y==width || x==0 || y==0)
-	{
-		end = 2;
-	}
-	if(x==xf && y == yf)
-		{
-		label3:
-		xf=rand()%height;
-		if(xf == 0)
-		goto label3;
-		label4:
-		yf=rand()%width;
-		if(yf == 0) 
-		goto label4;	
-		}
+
+void draw() {
+    system("cls"); 
+
+    for (int i = 0; i <= height; i++) {
+        for (int j = 0; j <= width; j++) {
+            if ((i == 0 || i == height) && (j == 0 || j == width))
+                printf("+");
+            else if (i == 0 || i == height)
+                printf("-");
+            else if (j == 0 || j == width)
+                printf("|");
+            else {
+                if (i == x && j == y)
+                    printf("O");
+                else if (i == xf && j == yf)
+                    printf("#");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    if (x == height || y == width || x == 0 || y == 0) {
+        end = 2;
+    }
+
+    if (x == xf && y == yf) {
+        do {
+            switch (rand() % 2) {
+                case 0:
+                    xf = rand() % height;
+                    if (xf != 0)
+                        break;
+                case 1:
+                    yf = rand() % width;
+                    if (yf != 0)
+                        break;
+            }
+            break;
+        } while (x == xf && y == yf);
+    }
 }
 
 void move()
